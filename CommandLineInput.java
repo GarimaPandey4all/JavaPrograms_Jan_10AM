f1a51ef1fc7a841a1ef9d29440442bb8cc56a5ce
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-01-20 12:51:19 +0530

    Add files via upload

--- CommandLineInput.java
+++ CommandLineInput.java
@@ -3,12 +3,20 @@ class CommandLineInput {
     public static void main(String[] args) {
 
         //dynamic way of initialization, command line input
-        
-        int firstNumber = Integer.parseInt(args[0]);
-        int secondNumber = Integer.parseInt(args[1]);
-        int result = firstNumber + secondNumber;
+        // if-else 
+        //Conditional Statements 
 
-        System.out.println("Sum is: "+result);
+        if(args.length == 2)
+        {
+            int firstNumber = Integer.parseInt(args[0]);
+            int secondNumber = Integer.parseInt(args[1]);
+            int result = firstNumber + secondNumber;
+
+            System.out.println("Sum is: "+result);
+        }
+        else {
+            System.out.println("Invalid Input");
+        }
 
     }
     
