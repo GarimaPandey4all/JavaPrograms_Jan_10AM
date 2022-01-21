dd060c3797a780e534e2abe299dc0df292d81261
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-01-21 11:05:08 +0530

    Add files via upload

--- LoopsInput.java
+++ LoopsInput.java
@@ -6,12 +6,23 @@ class LoopsInput {
             // Loop: Repeatition/Iteration
 
             //Traditional For Loop or Traditional Way of Loop
-            for(int i = 0; i < args.length; i++)
+            // for(int i = 0; i < args.length; i++)// i = 0, 1, 2, 3, 4
+            // {
+            //     sum = sum + Integer.parseInt(args[i]);// 100, 300, 600, 1000
+            // }
+
+            // for(int i = 1; i <= 20; i++)
+            // {
+            //     System.out.println("Hello World");
+            // }
+
+            //Enhanced For Loop, only it is used to read the array(args)
+            for(String value : args)
             {
-                sum = sum + Integer.parseInt(args[i]);
+                sum = sum + Integer.parseInt(value);
             }
 
-            System.out.println("Sum is: "+sum);
+            System.out.println("Sum is: "+sum); // 1000
 
     }    
 }
