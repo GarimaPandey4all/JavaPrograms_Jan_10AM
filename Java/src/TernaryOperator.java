1075f750fc71b0f2e86bdc5a3aa9e01cf7762ab3
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-02-11 12:20:39 +0530

    Add files via upload

---
+++ Java/src/TernaryOperator.java
@@ -0,0 +1,23 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		shorthand
+//		? : - Ternary Operator or conditional operator
+//				
+//		Ternary - 3
+//		
+//		(condition) ? true : false;
+		
+		int a = 1000, b = 200;
+		
+		//String result = (a > b) ? "A is Greater" : "B is Greater";
+		int result = (a > b) ? a : b;
+		
+		System.out.println("Greater Number is: "+result);
+
+	}
+
+}
