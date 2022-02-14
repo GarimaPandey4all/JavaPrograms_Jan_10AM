d1bc102ac009e2c5338512b179eb0a8efc72d058
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-02-14 11:06:00 +0530

    Add files via upload

--- Java/src/oops/Student.java
+++ Java/src/oops/Student.java
@@ -9,17 +9,24 @@ public class Student {
 	private String phone;
 	private String course;
 	private double fees;
+	private String collegeName;
 	
+	//default constructor
+	Student(){
+		//Call Parametrized Constructor
+		this(1001, "Ram Kumar", "812364689", "MCA", 10000);
+		collegeName = "SRCC";
+	}
 	
-	// r, n, c, p, f = local variables
-	public void takeInput(int r, String n, String p, String c, double f) {
-		
-//		instance variable = local variable
-		rollno = r;
-		name = n;
-		phone = p;
-		course = c;
-		fees = f;
+	//parameterized constructor
+	Student(int rollno, String name, String phone, String course, double fees)
+	{
+		//this(); // call default constructor
+		this.rollno = rollno;
+		this.name = name;
+		this.phone = phone;
+		this.course = course;
+		this.fees = fees;
 	}
 
 	public void print()
@@ -29,14 +36,33 @@ public class Student {
 		System.out.println("Phone is: "+phone);
 		System.out.println("Course is: "+course);
 		System.out.println("Fees is: "+fees);
+		System.out.println("College Name is: "+collegeName);
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Student ram = new Student(); // new - is used to allocate memory at runtime
-		ram.takeInput(1001, "Ram Kumar", "812364689", "MCA", 10000);
+		//Call Parameterized Constructor
+//		Student ram = new Student(1001, "Ram Kumar", "812364689", "MCA", 10000);	
+		
+		Student ram = new Student(); // Call default constructor 
+		
+		// new - is used to allocate memory at runtime
+		//ram.takeInput(1001, "Ram Kumar", "812364689", "MCA", 10000);
+		
 		ram.print();
+		
+//		ram.setPhone("8374987345");
+//		ram.print();
+//		System.out.println(ram.getPhone());
 		
 //		System.out.println("Roll no is: "+ram.rollno);
 //		System.out.println("Name is: "+ram.name);
@@ -60,5 +86,23 @@ public class Student {
 //		System.out.println("Course is: "+ram.course);
 //		System.out.println("Fees is: "+ram.fees);
 	}
+	
+	// r, n, c, p, f = local variables
+		//public void takeInput(int r, String n, String p, String c, double f) {
+//		public void takeInput(int rollno, String name, String phone, String course, double fees) {
+////			instance variable = local variable
+//			
+//			this.rollno = rollno;
+//			this.name = name;
+//			this.phone = phone;
+//			this.course = course;
+//			this.fees = fees;	
+	//
+////			rollno = r;
+////			name = n;
+////			phone = p;
+////			course = c;
+////			fees = f;
+//		}
 
 }
